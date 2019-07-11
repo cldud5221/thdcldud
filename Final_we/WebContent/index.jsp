7ecaa521cf231d71e6500da4c24eee664bf92359
thdcldud <cldud5221@naver.com>
2019-07-11 20:39:24 +0900

    init

---
+++ Final_we/WebContent/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title> 3days의 웹 페이지</title>
+</head>
+<body>
+	L O G I N   P A G E
+	<form action="./userjoinAction.jsp" method="post">
+		<input type="text" name="userID">
+		<input type="password" name="userPassword">
+		<input type="submit" value="회원가입">
+	</form>
+</body>
+</html>
---
+++ info/src/util/DatabaseUtil.java
@@ -0,0 +1,21 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseUtil {
+
+	public static Connection getConnection() {
+		try {
+			String dbURL = "jdbc:mysql://200.200.200.31:3306/Lecture Evaluation";
+			String dbID = "root";
+			String dbPassword = "keroro2424.";
+			Class.forName("com.mysql.jdbc.Driver");
+			return DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+		
+}
